e188664257fae55de2ae46e6129b71c2abdc5724
kumar <kumarrajavasiraju@gmail.com>
2024-12-07 13:37:02 +0530

    VKR | UPDATE App.js

--- app.js
+++ app.js
@@ -1,29 +1,45 @@
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const taskRoutes = require('./routes/taskRoutes');
+const mainRoutes = require('./routes/taskRoutes');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/api', taskRoutes);
+const PORT = process.env.PORT || 3800;
 
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
-  const details = err.details || null;
-  res.status(statusCode).json({success: false, message,details });
-});
-
+// MongoDB connection
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('MongoDB Connected...');
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on http://localhost:${process.env.PORT}`);
-    });
+    console.log('Successfully connected to the database.');
   })
-  .catch((err) => {
-    console.error(err.message);
+  .catch((error) => {
+    console.error('Could not connect to the database:', error.message);
+    process.exit(1); // Exit process on DB connection failure
   });
+
+// Routes
+app.use(mainRoutes);
+
+// Global Error Handling Middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500; // Default to 500 (Internal Server Error)
+  const message = err.message || 'Internal Server Error';
+  const details = err.details || null;
+
+  console.error(`Error: ${message}`);
+  res.status(statusCode).json({
+    success: false,
+    message,
+    details,
+  });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
